2a3af3edab31ab1b1c91f4b9b8c7132e8b20ef6e
iMustakaev <ildar.mustakaev@gmail.com>
2019-12-22 16:51:48 +0300

    add sqArea tests

--- src/SqArea/SqArea.java
+++ src/SqArea/SqArea.java
@@ -1,4 +1,4 @@
-package com.company;
+package SqArea;
 
 public class SqArea {
     public static double square(int p, int k) {
@@ -9,6 +9,6 @@ public class SqArea {
 
     public static void main(String[] args) {
         System.out.println(" p = 4, k = 1, s = 1, real = " + square(4,1));
-        System.out.println(" p = 6, k = 2, s = 1, real = " + square(6,2));
+        System.out.println(" p = 6, k = 2, s = 2, real = " + square(6,2));
     }
 }
