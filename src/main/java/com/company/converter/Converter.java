4c7a0ff649ec3331e6418db21439160e87317583
iMustakaev <ildar.mustakaev@gmail.com>
2019-12-22 19:35:08 +0300

    add tests dependency

--- src/main/java/com/company/converter/Converter.java
+++ src/main/java/com/company/converter/Converter.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.company.converter;
 
 public class Converter {
 
---
+++ src/main/java/test/java/SqAreaTest.java
@@ -0,0 +1,13 @@
+//package SqArea;
+//
+//import Point.Point;
+//import org.junit.Assert;
+//import org.junit.Test;
+//
+//public class SqAreaTest {
+//    @Test
+//    public void distance() {
+//        Assert.assertEquals(1, SqArea.square(4,1), 0.01);
+//        Assert.assertEquals(2, SqArea.square(6,2), 0.01);
+//    }
+//}
