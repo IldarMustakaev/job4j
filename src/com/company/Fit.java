a27777c67d4add181b543c34a6ff7cf56d65712e
iMustakaev <ildar.mustakaev@gmail.com>
2019-12-22 14:55:49 +0300

    first commit. last changes add tests

---
+++ src/com/company/Fit.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Fit {
+
+    public static double manWeight(double height) {
+        return (height - 100)*1.15;
+    }
+
+    public static double womanWeight(double height) {
+        return (height - 110)*1.15;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Man 180 is " + manWeight(180));
+        System.out.println("Woman 170 is " + womanWeight(170));
+    }
+}
