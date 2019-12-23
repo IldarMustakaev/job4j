70ed705232cb9eead27e73605dc6926ec6688072
i-mustakaev <i.mustakaev@qiwi.com>
2019-12-23 11:54:29 +0300

    add checkstyle and change code after audit

--- chapter_001/src/main/java/com/company/Fit.java
+++ chapter_001/src/main/java/com/company/Fit.java
@@ -3,11 +3,11 @@ package com.company;
 public class Fit {
 
     public static double manWeight(double height) {
-        return (height - 100)*1.15;
+        return (height - 100) * 1.15;
     }
 
     public static double womanWeight(double height) {
-        return (height - 110)*1.15;
+        return (height - 110) * 1.15;
     }
 
     public static void main(String[] args) {
--- chapter_001/src/test/java/SqAreaTest.java
+++ chapter_001/src/test/java/SqAreaTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class SqAreaTest {
     @Test
     public void distance() {
-        Assert.assertEquals(1, SqArea.square(4,1), 0.01);
-        Assert.assertEquals(2, SqArea.square(6,2), 0.01);
+        Assert.assertEquals(1, SqArea.square(4, 1), 0.01);
+        Assert.assertEquals(2, SqArea.square(6, 2), 0.01);
     }
 }
